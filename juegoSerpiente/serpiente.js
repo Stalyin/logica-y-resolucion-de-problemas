7439c733c8a059c65de06839ee7f0e7dbb41ba92
Stalyin <stalyin16ac@gmail.com>
2026-05-07 21:12:39 -0500

    feat:Add the line draw in the game

--- juegoSerpiente/serpiente.js
+++ juegoSerpiente/serpiente.js
@@ -1,25 +1,38 @@
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+const TAMANIO_CELDA = 25;
 
+function dibujarTablero() {
+  ctx.strokeStyle = "#f70000";
+  for (let y = 0; y <= canvas.height; y += TAMANIO_CELDA) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+  }
 
-    
+  for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.stroke();
+  }
+}
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+// Primera pintura del juego al cargar la página
+dibujarTodo();
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+// =========================
+// FUNCIONES DE DIBUJO
+// =========================
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
-
-
-
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero();
+}
